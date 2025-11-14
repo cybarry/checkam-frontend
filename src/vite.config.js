376915292ded33ac5f6d605806df28e40be7f51a
cybarry <abdulbarrshonibare@yahoo.com>
2025-11-14 09:21:32 +0100

    feat: add .

--- src/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// This is the ONLY code that should be in this file.
-export default defineConfig({
-  plugins: [react()],
-})
